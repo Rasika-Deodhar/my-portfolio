b02458f6925d39cf3cb73b21e56198be9837c62c
Rasika Deodhar <rasikadeodhar95@gmail.com>
2024-10-02 00:42:04 -0500

    Fixing the CSS; TODO: still more to fix

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -9,15 +9,23 @@ import {
   useTheme,
 } from '@mui/material'
 import ReactCardFlip from 'react-card-flip'
-import BraggedImage from '../../images/Bragged_pic.png'
-import GitpitImage from '../../images/Gitpit_pic.png'
-import XImage from '../../images/X_pic.png'
+import BraggedImage from '../../images/B1.png'
+import GitpitImage from '../../images/G1.png'
+import XImage from '../../images/X1.png'
 // @ts-ignore
 
 interface contactProps {}
 
 const Contact: FC<contactProps> = () => {
-  const theme = useTheme()
+
+  const CardStyle = {
+    border: "1px solid black",
+    padding: "20px",
+    margin: "20px",
+    width: "350px",
+    height: "250px",
+    fontFamily : 'Chillax'
+  };
 
   const [isLinkedinFlipped, setIsisLinkedinFlipped] = useState(false)
   const [isGithubFlipped, setIsisGithubFlipped] = useState(false)
@@ -33,64 +41,76 @@ const Contact: FC<contactProps> = () => {
   }
 
   return (
-    <div className='contact' style={{display:'flex'}}>
+    <div className='contact' style={{display:'flex', justifyContent:'center'}}>
       <ReactCardFlip isFlipped={isLinkedinFlipped} flipDirection='horizontal' flipSpeedBackToFront={0.9} cardZIndex='auto'>
-        <Card sx={{ maxWidth: 345, marginLeft: '100px' }}>
+        <Card style={CardStyle}>
           <CardActionArea onClick={handleLinkedinClick}>
-            <CardMedia component='img' image={BraggedImage} alt='braggedin' />
+            <CardMedia component='img' image={BraggedImage} alt='braggedin'  />
             <CardContent>
-              <Typography gutterBottom variant='h5' component='div'>
-                For all the subtle flex and self-promotion.
+              <Typography gutterBottom variant='h5' component='div' fontFamily={'Chillax'}>
+                For all the subtle flex and self-promotion. 😎
               </Typography>
             </CardContent>
           </CardActionArea>
         </Card>
 
-        <Card sx={{ maxWidth: 345, marginLeft: '100px' }}>
+        <Card style={CardStyle}>
           <CardActionArea onClick={handleLinkedinClick}>
             <CardMedia
               component='img'
               image='https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Logo.svg.original.svg'
               alt='linkedin'
-              style={{ marginLeft: '1px' }}
-            />
-            <CardContent>
-              <Typography
+              style={{ marginLeft: '1px', height: 'auto' }}
+            />            
+            <div style={{height:'100px'}}></div>
+            <Typography
                 gutterBottom
                 variant='h4'
                 component='a'
                 fontStyle='large'
                 href='https://www.linkedin.com/in/rasika-deodhar/'
                 target='blank'
+                fontFamily={'Chillax'}
+                sx={{
+                  textDecoration: 'none',
+                  margin: '16px 0',
+                  color: 'black',
+                  transition: 'color 0.3s ease, transform 0.3s ease',
+                  '&:hover': {
+                    color: '#1976d2 !important', // Change to any hover color you like
+                    transform: 'scale(1.05)', // Slightly increase the size on hover
+                    textDecoration: 'underline',
+                  },
+                }}
               >
-                Let's connet!
+                Let's connect!
               </Typography>
-            </CardContent>
           </CardActionArea>
         </Card>
       </ReactCardFlip>
       
       <ReactCardFlip isFlipped={isGithubFlipped} flipDirection='horizontal' flipSpeedBackToFront={0.9} cardZIndex='auto'>
-        <Card sx={{ maxWidth: 345, marginLeft: '100px' }}>
+        <Card style={CardStyle}>
           <CardActionArea onClick={handleGithubClick}>
-            <CardMedia component='img' image={GitpitImage} alt='braggedin' />
+            <CardMedia component='img' image={GitpitImage} alt='Github' />
             <CardContent>
-              <Typography gutterBottom variant='h5' component='div'>
-                The place where all your abandoned projects fall into.
+              <Typography gutterBottom variant='h5' component='div' fontFamily={'Chillax'}>
+                Place where all abandoned projects fall into. 🥲
               </Typography>
             </CardContent>
           </CardActionArea>
         </Card>
 
-        <Card sx={{ maxWidth: 345, marginLeft: '100px' }}>
+        <Card style={CardStyle}>
           <CardActionArea onClick={handleGithubClick}>
             <CardMedia
               component='img'
               image='https://github.githubassets.com/assets/GitHub-Logo-ee398b662d42.png'
               alt='github'
-              style={{ marginLeft: '1px' }}
+              style={{ marginLeft: '1px', height: 'auto' }}
             />
             <CardContent>
+              <div style={{height:'80px'}}></div>
               <Typography
                 gutterBottom
                 variant='h4'
@@ -98,8 +118,19 @@ const Contact: FC<contactProps> = () => {
                 fontStyle='large'
                 href='https://github.com/Rasika-Deodhar'
                 target='blank'
+                fontFamily={'Chillax'}
+                sx={{
+                  textDecoration: 'none',
+                  color: 'black',
+                  transition: 'color 0.3s ease, transform 0.3s ease',
+                  '&:hover': {
+                    color: '#1976d2 !important', // Change to any hover color you like
+                    transform: 'scale(1.05)', // Slightly increase the size on hover
+                    textDecoration: 'underline',
+                  },
+                }}
               >
-                Let's connet!
+                Let's connect!
               </Typography>
             </CardContent>
           </CardActionArea>
@@ -107,18 +138,18 @@ const Contact: FC<contactProps> = () => {
       </ReactCardFlip>
 
       <ReactCardFlip isFlipped={isXFlipped} flipDirection='horizontal' flipSpeedBackToFront={0.9} cardZIndex='auto'>
-        <Card sx={{ maxWidth: 345, marginLeft: '100px' }}>
+        <Card style={CardStyle}>
           <CardActionArea onClick={handleXClick}>
-            <CardMedia component='img' image={XImage} alt='braggedin' />
+            <CardMedia component='img' image={XImage} alt='X' />
             <CardContent>
-              <Typography gutterBottom variant='h5' component='div'>
-                Because we’re all just caffeinated coders sharing thoughts.
+              <Typography gutterBottom variant='h5' component='div' fontFamily={'Chillax'}>
+                Caffeinated coders sharing thoughts. 🙃
               </Typography>
             </CardContent>
           </CardActionArea>
         </Card>
 
-        <Card sx={{ maxWidth: 345, marginLeft: '100px' }}>
+        <Card style={CardStyle}>
           <CardActionArea onClick={handleXClick}>
             <CardMedia
               component='img'
@@ -127,6 +158,7 @@ const Contact: FC<contactProps> = () => {
               style={{ marginLeft: '1px', height: '150px' }}
             />
             <CardContent>
+              <div style={{height:'25px'}}></div>
               <Typography
                 gutterBottom
                 variant='h4'
@@ -134,15 +166,14 @@ const Contact: FC<contactProps> = () => {
                 fontStyle='large'
                 href='https://x.com/MoodLearner'
                 target='blank'
+                fontFamily={'Chillax'}
               >
-                Let's connet!
+                Let's connect!
               </Typography>
             </CardContent>
           </CardActionArea>
         </Card>
       </ReactCardFlip>
-
-
     </div>
   )
 }
