6fc338b5678dfeb34a7a642d0891ab73bc313760
Rasika Deodhar <rasikadeodhar95@gmail.com>
2024-10-04 15:33:46 -0500

    created spiral for skills

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,23 +1,35 @@
 import React, { FC } from 'react';
 import './skills.css';
 
-interface skillsProps {}
+interface SkillsProps {}
 
-const skills: FC<skillsProps> = () => {
+const Skills: FC<SkillsProps> = () => {
+   const numPoints = 350; // Number of points along the spiral
+   const spiralPath = [];
 
-const Circle: React.FC<{ size: string | number; color: string }> = ({ size, color }) => {
-  return (
-    <div className="circle" style={{ width: size, height: size, backgroundColor: color }}></div>
-  );
-};    
-return(
- <div className='skills'>
-    <h1>TODO: Skills Component</h1>
-    <div>
-      <Circle size="50px" color="red" />
-      <Circle size="100px" color="blue" />
-    </div>
- </div>)
+   // Generate points for secondary (overlay) spiral
+   for (let i = 0; i < numPoints; i++) {
+     const angle = 0.1 * i;
+     const radius = 0.9 * i; // Slight offset for the overlay spiral, and adjusted size
+     const x = radius * Math.cos(angle);
+     const y = radius * Math.sin(angle);
+     spiralPath.push(`${x},${y}`);
+   }
+ 
+   const spiralPoints = spiralPath.join(" ");
+ 
+   return (
+     <div className="svg-container circle skills">
+       <svg viewBox="-600 -600 1200 1200" xmlns="http://www.w3.org/2000/svg">
+         <polyline
+           points={spiralPoints}
+           stroke="#D3A588"  /* Lighter color for the overlay spiral */
+           strokeWidth="50"
+           fill="none"
+         />
+       </svg>
+     </div>
+   );
 };
 
-export default skills;
+export default Skills;
