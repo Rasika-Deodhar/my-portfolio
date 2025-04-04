2163ca0b1a10a409c57b2e21a838e5b0b629bb7e
Rasika Deodhar <rasikadeodhar95@gmail.com>
2025-04-04 18:57:08 -0500

    Note on project page

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -4,7 +4,13 @@ import "./projects.css";
 interface projectsProps {}
 
 const Projects: FC<projectsProps> = () => {
-  return <div>Heya! This is a work in progress...</div>;
+  return (
+    <div className="projects">
+      <center>
+        <h2>This page is a work in progress. Stay tuned for updates!</h2>
+      </center>
+    </div>
+  );
 };
 
 export default Projects;
